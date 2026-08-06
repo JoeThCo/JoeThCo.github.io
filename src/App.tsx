b29d8f6b046797dbd285fad03b4d8dce08bfe23f
Joe Colley <josephthco@gmail.com>
2026-08-06 11:39:13 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,9 @@ function App() {
         <div>
           <h2>Joe Colley! Wowza!</h2>
           <h2>This is clearly under construction!</h2>
+          <h3>
+            If you want to offer me a job, email me at josephthco@gmail.com
+          </h3>
         </div>
       </section>
     </>
